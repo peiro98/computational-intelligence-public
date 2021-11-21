c5a066d52bf5bced2795d730553ce577a47ad226
Simone Alberto Peirone <simone.peirone.98@gmail.com>
2021-11-21 23:48:10 +0100

    Fix visualization issues and add more documentation

--- connect-four/utils.py
+++ connect-four/utils.py
@@ -1,33 +1,33 @@
 import functools
 
 
-def pretty_print_board(board, col_height, num_cols, p1="🟡", p2="🔴"):
+def pretty_print_board(board, col_height, num_cols, p1="X", p2="O"):
     """
     Pretty print the Connect4 board
     """
     players = {
         1: p1,
         -1: p2,
-        0: "  "
+        0: " "
     }
 
     print(f"P1: {p1}\tP2: {p2}", end='\n')
 
     # print header
-    print("  ╔" + "════╦" * (num_cols - 1) + "════╗")
+    print("  ╔" + "═════╦" * (num_cols - 1) + "═════╗")
     for y in range(col_height):
         if y > 0:
-            print("  ╠" + "════╬" * (num_cols - 1) + "════╣")
+            print("  ╠" + "═════╬" * (num_cols - 1) + "═════╣")
         for x in range(num_cols):
             if x == 0:
                 print(f"{col_height - y - 1} ║", end="")
 
             print(
-                f" {players[board[x, col_height - y - 1]]} " + "║", end="")
+                f"  {players[board[x, col_height - y - 1]]}  " + "║", end="")
         print()
 
-    print("  ╚" + "════╩" * (num_cols - 1) + "════╝")
-    print("   " + "".join(map(lambda i: f"  {i}  ", range(num_cols))) + " ")
+    print("  ╚" + "═════╩" * (num_cols - 1) + "═════╝")
+    print("   " + "".join(map(lambda i: f"  {i}   ", range(num_cols))) + " ")
 
 
 def counted(f):
